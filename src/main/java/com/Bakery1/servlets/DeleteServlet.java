6bc5e28bcc79fb6044f846bbfada74b2437296b4
IT24100651 <IT24100651@my.sliit.lk>
2025-05-19 10:42:25 +0530

    Add files via upload

---
+++ src/main/java/com/Bakery1/servlets/DeleteServlet.java
@@ -0,0 +1,38 @@
+package com.Bakery1.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+
+    private static final String FILE_NAME = "C:\\Users\\USER\\Desktop\\projectfinal\\cakeshop\\src\\main\\webapp\\WEB-INF\\users.txt";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String email = request.getParameter("email");
+
+        String filePath =  FILE_NAME;
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.contains(email)) {
+                    lines.add(line);
+                }
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (String l : lines) {
+                writer.write(l);
+                writer.newLine();
+            }
+        }
+
+        request.getSession().invalidate(); // log out
+        response.sendRedirect("delete1.jsp?message=Account deleted successfully.");
+    }
+}
